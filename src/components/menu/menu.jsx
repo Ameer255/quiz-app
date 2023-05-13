bf82f78be7889e86f245e341e7d7c4614919288d
Ameer255 <ameerhaider2553@gmail.com>
2023-05-13 14:34:26 +0500

    new changes made

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,16 +1,21 @@
 import { QuizBox } from "./quizMenu";
+import img1 from "../../assets/images/sscode.png";
+import img2 from "../../assets/images/Screenshot (51).png";
+import img3 from "../../assets/images/Screenshot (52).png";
+import img4 from "../../assets/images/Screenshot (53).png";
+
 
 export const Menu = () => {
   return (
     <div className="menu">
 
-    <QuizBox linkTo="/web-quiz" icon="fas fa-laptop-code" name=" Web Quiz"/>
+    <QuizBox linkTo="/web-quiz" img={img1} name=" Web Quiz"/>
 
-    <QuizBox linkTo="/gen-quiz" icon="fas fa-brain" name="IQ Quiz"/>
+    <QuizBox linkTo="/gen-quiz" img={img2} name="IQ Quiz"/>
 
-    <QuizBox linkTo="/english-quiz" icon="fas fa-spell-check" name="English & Grammer"/>
+    <QuizBox linkTo="/english-quiz" img={img3} name="English & Grammer"/>
 
-    <QuizBox linkTo="/math-quiz" icon="fas fa-calculator" name="Maths"/>
+    <QuizBox linkTo="/math-quiz" img={img4} name="Maths"/>
     
 
     </div>
--- src/components/menu/quizMenu.jsx
+++ src/components/menu/quizMenu.jsx
@@ -1,13 +1,15 @@
 import {React} from "react";
 import { Link } from "react-router-dom";
 
-export const QuizBox =({linkTo, icon, name})=>{
+
+
+export const QuizBox =({linkTo, img, name})=>{
 
     return(
         <div className="box">
         <Link to={linkTo}>
           <div className="icon">
-            <i className={icon}></i>
+            <img src={img} alt="quiz img"/>
           </div>
           <div className="quiz-name">
             <b>{name}</b>
